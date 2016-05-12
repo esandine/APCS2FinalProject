bf474f4fd2f93c3a7e187010af01eb840b1433b1
esandine <esandine@stuy.edu>
2016-05-12 12:14:21 -0400

    Started loading pixels

--- ReadImage.java
+++ ReadImage.java
@@ -2,17 +2,24 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 public class ReadImage{
-    public static void ReadImage(String input){
+    private BufferedImage image;
+    private int[][] pixelRBGValues;
+    /*private void loadRBGValues(){
+	pixelRGBValues = new int[image
+	for(int i = 
+	}*/
+    public ReadImage(String input){
 	try{
-	    BufferedImage i1 = ImageIO.read(new File(input));
+	    image = ImageIO.read(new File(input));
 	}catch(IOException e){
 	    System.out.println("No file found");
+	    image = null;
 	}
 	//i1=ImageIO.read(new File(input));
     }
     public static void main(String[]args){
 	if(args.length>0){
-	    ReadImage(args[0]);
+	    ReadImage r1 = new ReadImage(args[0]);
 	}
     }
 }
