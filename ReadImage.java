85a8d99e735243e1af9d10cd0a8892b5c4b9525e
Issac Kim <ikim1@stuy.edu>
2016-05-13 12:17:14 -0400

    start loadRGBValue

--- ReadImage.java
+++ ReadImage.java
@@ -3,23 +3,46 @@ import java.io.*;
 import javax.imageio.ImageIO;
 public class ReadImage{
     private BufferedImage image;
-    private int[][] pixelRBGValues;
-    /*private void loadRBGValues(){
-	pixelRGBValues = new int[image
-	for(int i = 
-	}*/
+    private int[][] pixelRGBValues;
+    private int height;
+    private int width;
+    private void loadRGBValues(){
+	pixelRGBValues = new int[height][width];
+	for(int r = 0; r < height; r++){ 
+	    for(int c = 0; c < width; c++){
+		pixelRGBValues[r][width] = image.getRGB(r, width - c - 1);
+	    }
+	}
+    }
     public ReadImage(String input){
 	try{
 	    image = ImageIO.read(new File(input));
+	    height = image.getHeight();
+	    width = image.getWidth();
 	}catch(IOException e){
 	    System.out.println("No file found");
 	    image = null;
 	}
 	//i1=ImageIO.read(new File(input));
     }
+    public String toString(){
+	String ans = "";
+	for(int r = 0; r < height; r++){
+	    for(int c = 0; c < width; c++){
+		ans += pixelRGBValues[r][c] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+    public String getDimension(){
+	return "h: " + height + ", w: " + width;
+    }
     public static void main(String[]args){
 	if(args.length>0){
 	    ReadImage r1 = new ReadImage(args[0]);
+	    System.out.println(r1.getDimension());
+	    // System.out.println(r1);
 	}
     }
 }
