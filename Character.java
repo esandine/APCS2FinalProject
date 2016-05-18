d6d07720b7181b3445ff7292ccf1e8ea59025c29
Issac Kim <ikim1@stuy.edu>
2016-05-17 21:18:13 -0400

    try to bug fix

--- Character.java
+++ Character.java
@@ -2,9 +2,10 @@ public class Character{
     private ReadImage character;
     public Character(String img){
 	character = new ReadImage(img);
-	character.setBlackAndWhite();
     }
     public void recolorImg(String newImage){
+	character.getDimension();
+	character.setBlackAndWhite();
 	character.outPut(newImage);
     }
 }
