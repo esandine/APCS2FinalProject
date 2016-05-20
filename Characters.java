5a7604def2f8b85234dfd52bffdf4ed3c85f2ea1
Issac Kim <ikim1@stuy.edu>
2016-05-20 00:53:26 -0400

    test with new dirs

--- Characters.java
+++ Characters.java
@@ -2,18 +2,23 @@ import java.io.File;
 
 public class Characters{
     private ReadImage[] characters;
+    private String directory;
     public Characters(String dir){
+	this(dir, "results/");
+    }
+    public Characters(String dir, String outputDir){
 	File d = new File(dir);
 	String[] charImages = d.list();
 	characters = new ReadImage[charImages.length];
 	for(int i = 0; i < characters.length; i++){
 	    characters[i] = new ReadImage(dir + charImages[i]);
 	}	
-	
+	File r = new File(outputDir);
+	directory = outputDir;
     }
     public void recolorDir(){
 	for(int i = 0; i < characters.length; i++){
-	    String outputFile = "char" + i;
+	    String outputFile = directory + "/char" + i + ".png";
 	    recolorImg(characters[i], outputFile);
 	}
     }
