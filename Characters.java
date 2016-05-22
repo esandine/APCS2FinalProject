eda72230e988f1dcb799a3b0f583355430cff0ba
Issac Kim <ikim1@stuy.edu>
2016-05-22 14:19:01 -0400

    add rescaling to Characters

--- Characters.java
+++ Characters.java
@@ -3,22 +3,34 @@ import java.io.File;
 public class Characters{
     private ReadImage[] characters;
     private String directory;
+    private String[] imgNames;
     public Characters(String dir){
 	this(dir, "results/");
     }
     public Characters(String dir, String outputDir){
 	File d = new File(dir);
 	String[] charImages = d.list();
+	imgNames = new String[charImages.length];
+	for(int i = 0; i < charImages.length; i++){
+	    imgNames[i] = charImages[i];
+	}
 	characters = new ReadImage[charImages.length];
 	for(int i = 0; i < characters.length; i++){
 	    characters[i] = new ReadImage(dir + charImages[i]);
-	}	
+	}
 	File r = new File(outputDir);
 	directory = outputDir;
     }
-    public void recolorDir(){
+    public void rescale(){
+	System.out.println("scaling images");
 	for(int i = 0; i < characters.length; i++){
-	    String outputFile = directory + "/char" + i + ".png";
+	    characters[i].scaleImage();
+	}
+    }
+    public void recolorDir(){
+	System.out.println("recoloring images to output");
+	for(int i = 0; i < characters.length; i++){
+	    String outputFile = directory + "/" + imgNames[i];
 	    recolorImg(characters[i], outputFile);
 	}
     }
@@ -26,4 +38,14 @@ public class Characters{
 	image.setBlackAndWhite();
 	image.outPut(outputFile);
     }
+    public static void main(String[] args){
+	
+	    Characters rescaling = new Characters("results/", "test/");
+	     rescaling.rescale();
+	    rescaling.recolorDir();
+	    
+
+    }
+	
+    
 }
--- tester.java
+++ tester.java
@@ -4,6 +4,7 @@ public class tester{
 	    booleanCharacters chars = new booleanCharacters(args[0]);
 	    ReadImage img = new ReadImage("testimage.png");
 	    img.setBlackAndWhite();
+	    img.scaleImage();
 	    booleanArray comp = new booleanArray(img.toBoolean());
 	    System.out.println(chars.compareTo(comp, 70));
 	}
