5561f8b96e9c958a5a6e902fb3adc233efd57df8
Issac-Kim <ikim1@stuy.edu>
2016-05-22 15:52:35 -0400

    fix issues with boolChars

--- booleanCharacters.java
+++ booleanCharacters.java
@@ -22,18 +22,23 @@ public class booleanCharacters{
 	System.out.println("making string values for images");
 	for(int i = 0; i < charImages.length; i++){
 	    charStrings[i] = charImages[i].substring(0, charImages[i].length() - 4);
+	    System.out.println(charStrings[i]);
+	    System.out.println(charImages[i]);
 	}
     }
     public boolean compareTo(booleanArray other, double percent){
 	for(int i = 0; i < booleanArrs.length; i++){
 	    if(booleanArrs[i].compareTo(other, percent)){
 		index = i;
+		System.out.println(index);
 		return true;
 	     
 	    }
 	}
 	return false;
     }
+
+    //returns the name of 
     public String toString(booleanArray other, double percent){
 	if(compareTo(other, percent)){
 	    return charStrings[index];
