1ed6234ee14c22c0383344b6b9f9b3762f2b408f
esandine <esandine@stuy.edu>
2016-05-22 17:30:57 -0400

    Added closestMatch() which finds the closest match to a booleanArray

--- tester.java
+++ tester.java
@@ -3,7 +3,7 @@ public class tester{
     public static void main(String[] args){
 	if(args.length > 0){
 	    booleanArray b = Read.loadBoolean(args[0]);
-	    b.loadDirectory(args[1]);
+	    b.loadDirectory("alphabet/");
 	    System.out.println(b.closestMatch());
 	}
     }
