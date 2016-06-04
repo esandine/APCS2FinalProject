b601ab08f4b70b844d39b25f805dee841c06806b
esandine <esandine@stuy.edu>
2016-06-04 16:11:37 -0400

    Commented Pixel.java

--- Pixel.java
+++ Pixel.java
@@ -8,29 +8,38 @@ public class Pixel{
     private int xcor;
     private int ycor;
     //Constructors
+
+    //Standard constructor
     public Pixel(Color c,int x, int y){
 	color = c;
 	xcor = x;
 	ycor = y;
 	setOpaque();
     }
+
+    //Makes a pixel from a pixel rgb value
     public Pixel(int RGB,int x,int y){
 	this(new Color(0),x,y);
 	setColor(convertPixel(RGB));
     }
-    //Mutators
+
+    //convertPixel converts colors from the BufferedImage rgb values to Color rgb values
     public int convertPixel(int RGB){
 	//ColorSpace space = new ICC_ColorSpace(ICC_Profile.getInstance(ColorSpace.CS_sRGB));
 	//ColorModel c = new ComponentColorModel(space, false, false, Transparency.OPAQUE, DataBuffer.TYPE_INT);
 	ColorModel c =  ColorModel.getRGBdefault();
 	return c.getRGB(RGB);
     }
+
+    //Mutators
     public void setColor(int rgb){
 	setColor(new Color(rgb));
     }
     public void setColor(Color c){
 	color = c;
     }
+
+    //Used to reset alpha values, no longer used
     public void setOpaque(){
 	if(color.getAlpha()<.5){
 	    color = new Color(color.getRed(),color.getGreen(),color.getBlue());
@@ -38,6 +47,7 @@ public class Pixel{
 	    color = Color.white;
 	}
     }
+
     public void setXcor(int x){
 	xcor = x;
     }
@@ -58,7 +68,7 @@ public class Pixel{
     public int getYcor(){
 	return ycor;
     }
-    //Sets the color ot black if it is black or white if it is any color that is not black
+    //Sets the color to white or black depending on the RGB values
     public void toBlackandWhite(){
 	if((color.getRed()<128)&&(color.getGreen()<128)&&(color.getBlue()<128)){
 	    setColor(Color.black);
