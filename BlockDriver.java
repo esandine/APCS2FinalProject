6bfc3b6cfe88bbabed1fdd03e7f85893d4c174b3
Issac-Kim <ikim1@stuy.edu>
2016-06-05 03:35:42 -0400

    alter blockreader to use booleanArrays instead of readimages

--- BlockDriver.java
+++ BlockDriver.java
@@ -2,8 +2,8 @@ public class BlockDriver{
     public static void main(String[] args){
 	BlockReader r = new BlockReader("cursor.png");
 	//r.setDef();
-	//	r.removeSymbols();
-	//r.makeImages();
+		r.removeSymbols();
+	r.makeImages();
 	System.out.println(r);
     }
 }
