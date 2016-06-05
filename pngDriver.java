d5656430402e6a27eabb21bf507ae2c2f8004d17
esandine <esandine@stuy.edu>
2016-06-05 11:12:04 -0400

    To Black and white now works by detecting most common and second most common color on most images

--- pngDriver.java
+++ pngDriver.java
@@ -1,6 +1,6 @@
 public class pngDriver{
     public static void main(String[]args){
-	booleanArray b = Read.loadBoolean("redA.jpeg");
+	booleanArray b = Read.loadBoolean(args[0]);
 	b.scale(50,50);
 	System.out.println(b);
     }
