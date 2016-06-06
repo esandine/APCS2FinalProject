748c3460f35e6375a1ceb318a1d3eea5abb3cfbc
Issac Kim <ikim1@stuy.edu>
2016-06-06 11:28:39 -0400

    rewrite demoDriver

--- DemoDriver.java
+++ DemoDriver.java
@@ -5,18 +5,31 @@ public class DemoDriver{
 	Scanner myScanner = new Scanner(System.in);
 	System.out.println("What image would you like to compare?");
 	String inputFile = "";
-	while(inputFile == ""){
+	boolean next = true;
+	while(inputFile.equals("")){
 	    try{
 		inputFile = myScanner.next();
+		ReadLines reader = new ReadLines(inputFile);
+		reader.divideLines();
+		System.out.println(reader.returnText());
+		System.out.println("Would you like another image read? y/n");
+		String response = "";
+		while(response.equals("")){
+		    response = myScanner.next();
+		    if(response.equals("y")){
+			inputFile = "";
+			System.out.println("Which image do you want to read next?");
+		    }
+		    else if(!response.equals("n")){
+			System.out.println("Answer with y or n only");
+			response = "";
+		    }
+		}
 	    }catch(NullPointerException e){
 		System.out.println("Choose another file");
 		inputFile = "";
 	    }
 	}
-	booleanArray myImage = Read.loadBoolean(inputFile);
-	myImage.loadCharacters();
-	System.out.println(myImage.closestMatch());
-	myImage.printChars();
     }
 }
 
