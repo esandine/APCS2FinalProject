78eb08bdf4d2071c068dabea3e10ae8ba2c096b3
esandine <esandine@stuy.edu>
2016-06-06 12:08:08 -0400

    problems with lowercase letters

--- BlockDriver.java
+++ BlockDriver.java
@@ -1,6 +1,6 @@
 public class BlockDriver{
     public static void main(String[] args){
-	ReadLines r = new ReadLines("alphabet.jpeg");
+	ReadLines r = new ReadLines("alpha2.gif");
 	//r.setDef();
 		r.divideLines();
 	
