70c44a54d3c462a1bd163569ddb53cdf6c06bb56
Issac-Kim <ikim1@stuy.edu>
2016-06-06 11:43:10 -0400

    add test img

--- BlockDriver.java
+++ BlockDriver.java
@@ -1,6 +1,6 @@
 public class BlockDriver{
     public static void main(String[] args){
-	ReadLines r = new ReadLines("NTest.jpeg");
+	ReadLines r = new ReadLines("Test4.jpeg");
 	//r.setDef();
 		r.divideLines();
 	
