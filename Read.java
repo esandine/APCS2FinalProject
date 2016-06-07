b9674df7db9797ac3717942f58a856c29bf499f8
Issac Kim <ikim1@stuy.edu>
2016-06-06 22:31:28 -0400

    process lowercase

--- Read.java
+++ Read.java
@@ -27,7 +27,7 @@ public class Read{
         String[] charImages = in.list();
 	for(int i = 0;i<charImages.length;i++){
 	    System.out.println(charImages[i]);
-	    rescale(dir+charImages[i]);
+	    rescale(dir+"/"+charImages[i]);
 	}
     }
 
@@ -46,7 +46,7 @@ public class Read{
         String[] charImages = in.list();
 	for(int i = 0;i<charImages.length;i++){
 	    System.out.println(charImages[i]);
-	    trim(dir+charImages[i]);
+	    trim(dir+"/"+charImages[i]);
 	}
     }
 
---
+++ lowerDriver.java
@@ -0,0 +1,5 @@
+public class lowerDriver{
+    public static void main(String[] args){
+	Read.processDir("OCRLower");
+    }
+}
